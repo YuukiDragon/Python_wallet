1271da76af31c8495489b147bbf408d3df456c76
Yuuki Konno <wittmannnicedylan@gmail.com>
2022-03-28 15:32:11 +0200

    seconde version finale du fichier

--- Exo/Portefeuille.py
+++ Exo/Portefeuille.py
@@ -1,35 +1,61 @@
 class Portefeuille:
 
-    def __init__(self, valeur, devise):
+    def __init__(self, valeur, devise, valeurd, devised):
         self.valeur = valeur
         self.devise = devise
+        self.valeurd = valeurd
+        self.devised = devised
 
-#Fonction Ajout d'argent
-    def ajouter (self, valeur, devise):
-        if self.devise==devise:
-            self.valeur=self.valeur+valeur
+    # self.valeure = valeure
+    # self.devisee = devisee
+
+    # Fonction Ajout d'argent euro
+    def ajouter(self, valeur, devise):
+        if self.devise == devise:
+            self.valeur = self.valeur + valeur
         else:
             print("Erreur de devise")
 
-#Fonction retrait argent
-    def retirer (self, valeur, devise):
-        if self.devise==devise:
-            self.valeur=self.valeur-valeur
+    # Fonction retrait argent euro
+    def retirer(self, valeur, devise):
+        if self.devise == devise:
+            self.valeur = self.valeur - valeur
+        else:
+            print("Erreur de devise")
+
+    # Fonction Ajout d'argent dollar
+    def ajouterd(self, valeurd, devised):
+        if self.devised == devised:
+            self.valeurd = self.valeurd + valeurd
+        else:
+            print("Erreur de devise")
+
+    # Fonction retrait argent dollar
+    def retirerd(self, valeurd, devised):
+        if self.devised == devised:
+            self.valeurd = self.valeurd - valeurd
         else:
             print("Erreur de devise")
 
     def __str__(self):
-        return "La valeur est égale à {} et la devise est égale à {}.".format(self.valeur, self.devise)
+        return "Votre portefeuille contient {} {} et {} {}.".format(self.valeur, self.devise, self.valeurd, self.devised)
 
-#Creation porte monnaie
-Portefeuille1 = Portefeuille(1000,"Euros")
 
+# Creation porte monnaie
+Portefeuille1 = Portefeuille(1000, "Euros", 250, "Dollar")
 
 # Ajout d'argent
-Portefeuille1.ajouter(int(input("Ajouter le montant souhaité")),"Euros")
+Portefeuille1.ajouter(int(input("Ajouter le montant souhaité")), "Euros")
 
 # Retrait D'argent
-Portefeuille1.retirer(int(input("Retirer le montant souhaité")),"Euros")
+Portefeuille1.retirer(int(input("Retirer le montant souhaité")), "Euros")
 
-#Affichage du porte monnaie
+# Ajout d'argent
+Portefeuille1.ajouterd(int(input("Ajouter le montant souhaité")), "Dollar")
+
+# Retrait D'argent
+Portefeuille1.retirerd(int(input("Retirer le montant souhaité")), "Dollar")
+
+# Affichage du porte monnaie
 print(Portefeuille1)
+

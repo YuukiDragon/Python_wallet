e7f3480e99b39e95a743dcbd2b06a71de93b9de7
Yuuki Konno <wittmannnicedylan@gmail.com>
2022-03-28 15:09:06 +0200

    push retrait

--- Exo/Portefeuille.py
+++ Exo/Portefeuille.py
@@ -4,9 +4,17 @@ class Portefeuille:
         self.valeur = valeur
         self.devise = devise
 
+#Fonction Ajout d'argent
     def ajouter (self, valeur, devise):
         if self.devise==devise:
             self.valeur=self.valeur+valeur
+        else:
+            print("Erreur de devise")
+
+#Fonction retrait argent
+    def retirer (self, valeur, devise):
+        if self.devise==devise:
+            self.valeur=self.valeur-valeur
         else:
             print("Erreur de devise")
 
@@ -14,11 +22,14 @@ class Portefeuille:
         return "La valeur est égale à {} et la devise est égale à {}.".format(self.valeur, self.devise)
 
 #Creation porte monnaie
-Portefeuille1 = Portefeuille(100,"Euros")
+Portefeuille1 = Portefeuille(1000,"Euros")
 
 
 # Ajout d'argent
 Portefeuille1.ajouter(int(input("Ajouter le montant souhaité")),"Euros")
+
+# Retrait D'argent
+Portefeuille1.retirer(int(input("Retirer le montant souhaité")),"Euros")
 
 #Affichage du porte monnaie
 print(Portefeuille1)

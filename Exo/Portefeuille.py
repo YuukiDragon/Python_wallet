5ddeaf0943f677c2359bc7d957cc1e49c911ab72
Yuuki Konno <wittmannnicedylan@gmail.com>
2022-03-28 15:01:05 +0200

    push print

--- Exo/Portefeuille.py
+++ Exo/Portefeuille.py
@@ -10,10 +10,15 @@ class Portefeuille:
         else:
             print("Erreur de devise")
 
+    def __str__(self):
+        return "La valeur est égale à {} et la devise est égale à {}.".format(self.valeur, self.devise)
 
 #Creation porte monnaie
 Portefeuille1 = Portefeuille(100,"Euros")
 
 
 # Ajout d'argent
-Portefeuille1.ajouter(int(input("Ajouter le montant souhaité")),"euros")
+Portefeuille1.ajouter(int(input("Ajouter le montant souhaité")),"Euros")
+
+#Affichage du porte monnaie
+print(Portefeuille1)
